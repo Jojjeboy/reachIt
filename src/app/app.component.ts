ac40ebf0f6d9508b455d6b5a5a981fb66ddc43b2
Johannes Kärnstam <snack@johanneskarnstam.se>
2018-11-18 21:29:28 +0100

    UUID

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from './local-storage.service';
+import { ReachIt } from './reach-it';
+import { UUIDService } from './uuid.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reachIt';
+
+  constructor(private localStorageService: LocalStorageService, private uuidService: UUIDService) {
+
+    const config = {
+      defaultData: [{
+        name: 'PushUp',
+        increseBy: 25,
+        decreseBy: 25,
+        resetEveryDay: true,
+        uuid: uuidService.UUID(),
+        value: 0
+      },
+      {
+        name: 'Ab-rolls',
+        increseBy: 10,
+        decreseBy: 10,
+        resetEveryDay: true,
+        uuid: uuidService.UUID(),
+        value: 0
+      },
+      ]
+    };
+  }
 }
---
+++ src/app/reach-it.ts
@@ -0,0 +1,8 @@
+export class ReachIt {
+    private name: string;
+    private increseBy: number;
+    private decreseBy: number;
+    private resetEveryDay: boolean;
+    private uuid: string;
+    private value: number;
+}
