3bd803a0c3c59e50dd18d6cdd5e0e26609af154c
Johannes Kärnstam <snack@johanneskarnstam.se>
2020-12-30 18:18:31 +0100

    adding App version to array and fix nonexestent prod add versions array

--- src/app/tally.service.ts
+++ src/app/tally.service.ts
@@ -213,8 +213,6 @@ export class TallyService {
       });
       this.localStorageService.saveConfig(config);
     }
-
-
   }
 
 }
--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,13 +1,13 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "257fd2a-dirty",
-    "hash": "257fd2a",
+    "dirty": false,
+    "raw": "25e494e",
+    "hash": "25e494e",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "257fd2a-dirty",
+    "suffix": "25e494e",
     "semverString": null,
     "version": "0.0.0"
 };
