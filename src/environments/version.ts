078d03f3e8c68d03f87ce97f551e1bd2507d1986
Johannes Kärnstam <snack@johanneskarnstam.se>
2021-01-02 17:35:56 +0100

    Postinstall autocommit message

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,15 +1,15 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "a8e3500-dirty",
-    "hash": "a8e3500",
+    "dirty": false,
+    "raw": "c68aad6",
+    "hash": "c68aad6",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "a8e3500-dirty",
+    "suffix": "c68aad6",
     "semverString": null,
     "version": "0.0.0",
-    "date": "2021-01-02T16:06:46.043Z"
+    "date": "2021-01-02T16:35:56.943Z"
 };
 /* tslint:enable */
